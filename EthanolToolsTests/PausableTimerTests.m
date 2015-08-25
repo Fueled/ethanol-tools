856aace27475ad676c5c4f63224885caa5c94477
Bastien Falcou <bastien@fueled.com>
2015-08-25 17:52:01 -0400

    feat(PausableTimerTests): improved asynchronous tests.

--- EthanolToolsTests/PausableTimerTests.m
+++ EthanolToolsTests/PausableTimerTests.m
@@ -22,68 +22,119 @@
 
 @implementation PausableTimerTests
 
-- (void)testTimeIntervalRunningWhenTimerInitializedNotPaused {
-  ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:nil paused:NO];
-  [NSThread sleepForTimeInterval:0.5f];
-  
-  XCTAssertNotEqual(timer.timeInterval, 5.0f);
-}
-
 - (void)testTimeIntervalNotRunningWhenTimerInitializedPaused {
-  ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:nil paused:YES];
-  [NSThread sleepForTimeInterval:0.5f];
+  XCTestExpectation *expectation = [self expectationWithDescription:@"TimeInterval Should Be Equal To Initial Time If Started Paused"];
   
-  XCTAssertEqual(timer.timeInterval, 5.0f);
+  ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:1.0f target:nil selector:nil userInfo:nil paused:YES];
+  [NSThread sleepForTimeInterval:0.5f];
+  if (timer.timeInterval == 1.0f) {
+    [expectation fulfill];
+  }
+  
+  [self waitForExpectationsWithTimeout:0.5f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testInstanceIntiializerSelector {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"TimeInterval Should Not Be Equal To Initial Time"];
+  
   ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:1.0f target:nil selector:nil userInfo:nil paused:NO];
   [NSThread sleepForTimeInterval:0.5f];
+  if (timer.timeInterval != 1.0f) {
+    [expectation fulfill];
+  }
   
-  XCTAssertNotEqual(timer.timeInterval, 1.0f);
+  [self waitForExpectationsWithTimeout:0.5f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testClassInitializerBlock {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"TimeInterval Should Not Be Equal To Initial Time"];
+  
   ETHPausableTimer *timer = [ETHPausableTimer timerWithTimeInterval:1.0f block:nil paused:NO];
   [NSThread sleepForTimeInterval:0.5f];
+  if (timer.timeInterval != 1.0f) {
+    [expectation fulfill];
+  }
   
-  XCTAssertNotEqual(timer.timeInterval, 1.0f);}
+  [self waitForExpectationsWithTimeout:0.5f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
+}
 
 - (void)testClassInitializerSelector {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"TimeInterval Should Not Be Equal To Initial Time"];
+  
   ETHPausableTimer *timer = [ETHPausableTimer timerWithTimeInterval:1.0f target:nil selector:nil userInfo:nil paused:NO];
   [NSThread sleepForTimeInterval:0.5f];
+  if (timer.timeInterval != 1.0f) {
+    [expectation fulfill];
+  }
   
-  XCTAssertNotEqual(timer.timeInterval, 1.0f);
+  [self waitForExpectationsWithTimeout:0.5f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testTimeIntervalIsTheRightNumberOfSeconds {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Timer Finished With Right Time"];
+  
   NSDate *beginDate = [NSDate date];
   ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:^(ETHPausableTimer *timer) {
     NSTimeInterval differenceSeconds = [[NSDate date] timeIntervalSinceDate:beginDate];
-    XCTAssert(differenceSeconds > 4.9f && differenceSeconds < 5.1f);
+    if (differenceSeconds > 4.9f && differenceSeconds < 5.1f) {
+      [expectation fulfill];
+    }
   } paused:YES];
   [timer resume];
+  
+  [self waitForExpectationsWithTimeout:6.0f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
+
 - (void)testTimerNotRunningWhenPaused {
-  NSDate *beginDate = [NSDate date];
-  ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:^(ETHPausableTimer *timer) {
-    NSTimeInterval differenceSeconds = [[NSDate date] timeIntervalSinceDate:beginDate];
-    XCTAssert(differenceSeconds > 4.9f && differenceSeconds < 5.1f);
-  } paused:NO];
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Timer Is Not Running When Paused"];
+
+  ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:nil paused:NO];
   
   [NSThread sleepForTimeInterval:2.0f];
   [timer pause];
   
   [NSThread sleepForTimeInterval:1.0f];
-  XCTAssert(timer.timeInterval > 2.9f && timer.timeInterval < 3.1f);
+  if (timer.timeInterval > 2.9f && timer.timeInterval < 3.1f) {
+    [expectation fulfill];
+  }
+  
+  [self waitForExpectationsWithTimeout:4.0f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testPause {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Timer Time Elapsed Correct Even If Has Been Paused"];
+
   NSDate *beginDate = [NSDate date];
   ETHPausableTimer *timer = [[ETHPausableTimer alloc] initWithTimeInterval:5.0f block:^(ETHPausableTimer *timer) {
     NSTimeInterval differenceSeconds = [[NSDate date] timeIntervalSinceDate:beginDate];
-    XCTAssert(differenceSeconds > 7.9f && differenceSeconds < 8.1f);
+    if (differenceSeconds > 7.9f && differenceSeconds < 8.1f) {
+      [expectation fulfill];
+    }
   } paused:NO];
   
   [NSThread sleepForTimeInterval:2.0f];
@@ -91,6 +142,12 @@
   
   [NSThread sleepForTimeInterval:3.0f];
   [timer resume];
+  
+  [self waitForExpectationsWithTimeout:9.0f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testNegativeTimeInterval {
@@ -101,22 +158,42 @@
 }
 
 - (void)testForcePauseInBackgroundTimer {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Timer Time Elapsed When Force Pause In Background"];
+
   ETHPausableTimer *timer = [ETHPausableTimer timerWithTimeInterval:5.0f block:nil paused:YES];
   timer.forcePauseInBackground = YES;
   [timer resume];
   
   [NSThread sleepForTimeInterval:2.0f];
-  XCTAssertLessThanOrEqual(timer.timeInterval, 3.0f);
+  if (timer.timeInterval <= 3.0f) {
+    [expectation fulfill];
+  }
+  
+  [self waitForExpectationsWithTimeout:4.0f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 - (void)testRedisableForcePauseInForegroundTimer {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Timer Time Elapsed When Force Pause Enabled / Disabled In Background"];
+  
   ETHPausableTimer *timer = [ETHPausableTimer timerWithTimeInterval:5.0f block:nil paused:YES];
   timer.forcePauseInBackground = YES;
   timer.forcePauseInBackground = NO;
   [timer resume];
   
   [NSThread sleepForTimeInterval:2.0f];
-  XCTAssertLessThanOrEqual(timer.timeInterval, 3.0f);
+  if (timer.timeInterval <= 3.0f) {
+    [expectation fulfill];
+  }
+  
+  [self waitForExpectationsWithTimeout:4.0f handler:^(NSError * _Nullable error) {
+    if (error) {
+      XCTFail(@"Expectation Failed with error: %@", error);
+    }
+  }];
 }
 
 @end
